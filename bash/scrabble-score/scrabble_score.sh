3c82c6f85da0460f8aae260ab47a8ebc57dfdc0c
David Andrews <andrews-d@hotmail.com>
2019-08-22 12:14:00 -0500

    fixed tabs - new version of vim

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -20,33 +20,33 @@ main(){
     local char="${str:${i}:1}"
 
     if [[ "${char}" =~ ${re_one} ]]; then
-			total="$((total + 1))"
-	  
-		elif [[ "${char}" =~ ${re_two} ]]; then
-			total="$((total + 2))"
+      total="$((total + 1))"
+    
+    elif [[ "${char}" =~ ${re_two} ]]; then
+      total="$((total + 2))"
 
-		elif [[ "${char}" =~ ${re_three} ]]; then
-			total="$((total + 3))"
+    elif [[ "${char}" =~ ${re_three} ]]; then
+      total="$((total + 3))"
 
-		elif [[ "${char}" =~ ${re_four} ]]; then
-			total="$((total + 4))"
+    elif [[ "${char}" =~ ${re_four} ]]; then
+      total="$((total + 4))"
 
-		elif [[ "${char}" =~ ${re_five} ]]; then
-			total="$((total + 5))"
+    elif [[ "${char}" =~ ${re_five} ]]; then
+      total="$((total + 5))"
 
-		elif [[ "${char}" =~ ${re_eight} ]]; then
-			total="$((total + 8))"
+    elif [[ "${char}" =~ ${re_eight} ]]; then
+      total="$((total + 8))"
 
-		elif [[ "${char}" =~ ${re_ten} ]]; then
-			total="$((total + 10))"
+    elif [[ "${char}" =~ ${re_ten} ]]; then
+      total="$((total + 10))"
 
-		else
-			continue
+    else
+      continue
     fi
   done
 
-	# Print the total
-	echo "${total}"
+  # Print the total
+  echo "${total}"
 }
 
 main "${@}"
